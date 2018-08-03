4b08cd83d8272b842427a6a84f2ac2cf461ad89e
henriquemalone <henrique.malone@gmail.com>
2018-08-03 10:08:26 -0300

    Initial commit

---
+++ app/src/main/java/aulaactivity/cursoandroid/com/aulaactivity/SegundaActivity.java
@@ -0,0 +1,54 @@
+package aulaactivity.cursoandroid.com.aulaactivity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SegundaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    public void recreate() {
+        super.recreate();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+    }
+}
